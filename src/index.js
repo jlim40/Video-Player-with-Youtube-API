1f775b959180c97c0ac2370e52737273dd3951f9
jlim <jihoon2514@gmail.com>
2018-01-24 01:39:15 -0500

    Setting up Environment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyC6Wd2pRuKprb9RmEPGTRyglYu8HbAElnI';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce some HTML
+const App = () => {
+  return <div>Hi!</div>;
+}
+// think of App as a factory (class)
+// To make an instance of it, make it JSX -> <App />
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
