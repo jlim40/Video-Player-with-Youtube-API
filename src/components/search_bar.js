7eea0c2e034a14c9e2e02910cb9458f487120898
jlim <jihoon2514@gmail.com>
2018-01-26 01:35:52 -0500

    Search_bar in progress - state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,30 @@
+import React, { Component } from 'react';
 
 
-const SearchBar = () => {
-  return <Input />
+// const SearchBar = () => {
+//   return <input />;
+// }
+
+//let's make it a class based components
+class SearchBar extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    )
+  }
+
+
+  // onInputChange(event) { //event Object
+  //   console.log(event.target.value);
+  // }
 }
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyC6Wd2pRuKprb9RmEPGTRyglYu8HbAElnI';
 
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
+
 // think of App as a factory (class)
 // To make an instance of it, make it JSX -> <App />
+
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
